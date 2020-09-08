0bd83e460e9ba4278567abdd7c34aa4748558bbd
SANKALPLUFFY <boradkarsankalp6@gmail.com>
2020-09-08 22:37:10 +0530

    Session filter

--- JIRAAPI/src/test/java/Utilities/Utilities.java
+++ JIRAAPI/src/test/java/Utilities/Utilities.java
@@ -8,7 +8,7 @@ public class Utilities
 	public static String addCommentJiraBoday(String comment)
 	{
 		String body = "{\r\n" + 
-				"		    \"body\":\r\n"+comment+" ,\r\n" + 
+				"		    \"body\":\""+comment+"\" ,\r\n" + 
 				"		    \"visibility\": {\r\n" + 
 				"		        \"type\": \"role\",\r\n" + 
 				"		        \"value\": \"Administrators\"\r\n" + 
@@ -18,14 +18,32 @@ public class Utilities
 		return body;
 	}
 	
-	public static String loginToJira(String userName, String Password)
+	public static String loginToJira(String UserName, String Password) 
 	{
 		String body = "{\r\n" + 
-				"    \"username\": \r\n"+userName+",\r\n" + 
-				"    \"password\": \r\n"+Password+"\r\n" + 
+				"    \"username\": \""+UserName+"\",\r\n" + 
+				"    \"password\": \""+Password+"\"\r\n" + 
 				"}";
 		
 		return body;
+	}
+	
+	public static String createIssue()
+	{
+		String s = "    { \"fields\": {\n" + 
+		  		"            \"project\":  {\n" + 
+		  		"                \"key\": \"JIR\"\n" + 
+		  		"            },\n" + 
+		  		"             \"summary\":\"something new...\",\n" + 
+		  		"              \"description\":\"descriptionhgghg\",\n" + 
+		  		"               \"issuetype\": {\n" + 
+		  		"                \"name\":\"Bug\"\n" + 
+		  		"            }\n" + 
+		  		"       }\n" + 
+		  		"    }\n" + 
+		  		" ";
+		
+		return s;
 	}
 	
 	public static String SessionID(String json, String field)
--- JIRAAPI/src/test/java/testCases/DefectRaising.java
+++ JIRAAPI/src/test/java/testCases/DefectRaising.java
@@ -1,43 +1,101 @@
 package testCases;
 
-import org.testng.annotations.Test;
 
 import Utilities.Utilities;
 
 import static io.restassured.RestAssured.*;
 
+import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
 
 public class DefectRaising 
 
 {
 	
+	public String SessionID;
+	
 	@Test
 	public void loginToJIRA() 
 	{
-		RestAssured.baseURI = "http://localhost:8080/";
-		
-		String sessionIDPath = "session.value";
-		
-		String SessionIDPOST = given()
-				
-				.header("Content-Type", "application/json")
-				
-				.body(Utilities.loginToJira("boradkarsankalp6", "@NEp|E(E1995")).log().all().
-		
-		when().post("rest/auth/1/session")
-		
-		.then().extract().response().asString();
-		
-		String SessionID = Utilities.SessionID(SessionIDPOST,sessionIDPath);
+		//login
 		
 		
-		given().log().all().pathParam("issueId", "12345")
+			RestAssured.baseURI = "http://localhost:8080/";
+			
+			String sessionIDPath = "session.value";
+			
+			SessionFilter session = new SessionFilter();
+			
+			//Initiating SessionFIlter class stores new created session which can be passed to any required functionality
+			
+			String SessionIDPOST = given().log().all()
+					
+			.header("Content-Type", "application/json")
+					
+			.body(Utilities.loginToJira("boradkarsankalp6","@NEp|E(E1995")).log().all()
+			
+			.filter(session)
+			
+			// here session will get stored such that we can pass thge session as "logged in user"
+			// to next required method which accepts generated session ID
+			// without parsing json response and extracting session ID
+			
+			.when().log().all().post("rest/auth/1/session")
+			
+			.then().log().all().extract().response().asString();
+			
+			// create session ID
+			
+			//SessionID = Utilities.SessionID(SessionIDPOST,sessionIDPath);
+			
+			//System.out.println(SessionID);
+			 
+					given()
+			.pathParam("ID", "10202")
+			.header("Content-type","application/json")
+			//.header("Cookie",SessionID)
+			.body("{\n" + 
+					"    \"body\": \"Adding comment to the issue\",\n" + 
+					"    \"visibility\": {\n" + 
+					"        \"type\": \"role\",\n" + 
+					"        \"value\": \"Administrators\"\n" + 
+					"    }\n" + 
+					"}")
+			.filter(session)
+			.when().log().all()
+			.post("rest/api/2/issue/{ID}/comment")
+			.then().log().all().assertThat().statusCode(201);
+			
+			// Creating issue from generated session ID
+			
+			/*String CreattIssuePost = given().log().all().
+					
+					header("Content-type","application/json")
+					
+					.header("Cookie",SessionID)
+					
+					.body(Utilities.createIssue())
+					
+					.when().post("rest/api/2/issue")
+					
+					.then().log().all().extract().response().asString();
+			
+			String IssueID = Utilities.SessionID(CreattIssuePost, "id");
+			
+			System.out.println(IssueID);*/
+			
+			
+			
+		
+		
+		/*given().log().all().pathParam("issueId", "12345")
 		
 		.header("Content-Type", "application/json")
 		
 		.body(Utilities.addCommentJiraBoday("adding a comment"))
 		
-		.when().post("rest/api/2/issue/{issueId}/comment");
+		.when().post("rest/api/2/issue/{issueId}/comment");*/
 	}
 }
